7df6ecfb6ee67bcc147d7d2ff19cfdba86cea86a
Gurpartap Singh <sgurpartap880@gmail.com>
2022-10-13 17:07:42 +0530

    completed the basic part of the game

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-// rock paper scissors game
-let game = ['Rock', 'Paper', 'Scissors'];
+let gameOptions = ["rock", "paper", "scissors"];
+
 
 function getComputerChoice() {
-    return game[Math.floor(Math.random()*game.length)];
+    return gameOptions[Math.floor(Math.random()*gameOptions.length)];
 }
+
+let computerSelection = getComputerChoice();
+console.log(" Computer Selected move : " + computerSelection); // give more clarity to the user 
+
+function getPlayerSelection() {
+    return prompt("Enter your move: rock, paper, scissors");
+}
+
+let playerSelection = getPlayerSelection();
+
+playerSelection = playerSelection.toLowerCase();
+console.log("Player selected move :" + playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    } else {
+        return "1";
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
